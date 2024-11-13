fb6817025bc416156c0a67611d5feb6e35c572b1
Alto0327 <aj.silvaf2003@gmail.com>
2024-11-13 18:19:55 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useRef } from "react"
+import './App.css'
+import Authentication from "./pages/Login-Signup/Authentication"
+
+function App() {
+
+
+  return (
+    <>
+    <Authentication/>
+    </>
+  )
+}
+
+export default App
