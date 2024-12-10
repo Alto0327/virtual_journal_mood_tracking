4840f1717345ffa130dbd521b4122b6593078752
Alto0327 <aj.silvaf2003@gmail.com>
2024-12-10 11:14:36 -0500

    moved journal list to side pannel

--- src/components/SidePanel/JournalsSearch.jsx
+++ src/components/SidePanel/JournalsSearch.jsx
@@ -1,6 +1,12 @@
-
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../../context/UserContext";
+import useJournal from "../../Hooks/useJournal";
 
 const JournalsSearch = () => {
+const { currentUser } = useContext(UserContext)
+const { entries, createJournalEntry, updateJournalEntry } = useJournal()
+
+
 
   return (
     <div className="journal-container">
@@ -8,12 +14,32 @@ const JournalsSearch = () => {
         <label>
           <h4>Search Journal</h4>
         </label>
-        <input
-
-        />
+        <input/>
+       
       </form>
       <div className="journal-entries">
-      
+      {currentUser !== null ? 
+          (
+          <div>
+            {entries.length > 0 ? 
+                (
+                  <ul>
+                    {entries.map(entry => (
+                      <li key={entry.id}>
+                        <h3>{entry.title}</h3>
+                        <small>{new Date(entry.date).toLocaleDateString()}</small>
+                      </li>
+                    ))}
+                  </ul>
+            ):(
+                <h1>no value</h1>
+            )}
+          </div>
+          ):(
+            <div>
+              <h4>Log in to View Journal Entries</h4>
+            </div>
+          )}
       </div>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import useJournal from '../../Hooks/useJournal'
 
 function Home() {
   const { currentUser } = useContext(UserContext)  
-  const { entries, createJournalEntry,updateJournalEntry } = useJournal()
+  const { entries, createJournalEntry, updateJournalEntry } = useJournal()
   const [newEntry, setNewEntry] = useState({ title: "", content: "", date: new Date().toISOString().split("T")[0] });
 
   const handleCreate = () => {
@@ -25,20 +25,7 @@ function Home() {
       {currentUser !== null ? 
         (
         <div>
-            <h1>Welcome in</h1>
-            {entries && entries.length > 0 ? 
-              (
-                <ul>
-                  {entries.map(entry => (
-                    <li key={entry.id}>
-                      <h3>{entry.title}</h3>
-                      <small>{new Date(entry.date).toLocaleDateString()}</small>
-                    </li>
-                  ))}
-                </ul>
-              ):(
-                <h1>no value</h1>
-            )}
+          <h1>Welcome in</h1>
           <input
             type="text"
             placeholder="Title"
